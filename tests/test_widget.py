f82e6782d535027f5dff888feb16550d6c2c83c6
Karol Gotkowski <karol.gotkowski@dkfz-heidelberg.de>
2025-12-10 10:42:32 +0100

    Updated tests

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,66 +1,33 @@
 import numpy as np
 
-from napari_stream._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
+from napari_stream._receiver_widget import (
+    ReceiverWidget,
+    bind_endpoint_for_public,
+    receiver_widget,
 )
 
 
-def test_threshold_autogenerate_widget():
-    # because our "widget" is a pure function, we can call it and
-    # test it independently of napari
-    im_data = np.random.random((100, 100))
-    thresholded = threshold_autogenerate_widget(im_data, 0.5)
-    assert thresholded.shape == im_data.shape
-    # etc.
+def test_bind_endpoint_for_public():
+    assert bind_endpoint_for_public("tcp://127.0.0.1:5556") == "tcp://*:5556"
+    assert bind_endpoint_for_public("tcp://*:5556") == "tcp://*:5556"
+    assert bind_endpoint_for_public("ipc:///tmp/napari_stream.sock") == "ipc:///tmp/napari_stream.sock"
 
 
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# you don't need to import it, as long as napari is installed
-# in your testing environment
-def test_threshold_magic_widget(make_napari_viewer):
+def test_receiver_widget_uses_existing_viewer(make_napari_viewer):
     viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-
-    # our widget will be a MagicFactory or FunctionGui instance
-    my_widget = threshold_magic_widget()
-
-    # if we "call" this object, it'll execute our function
-    thresholded = my_widget(viewer.layers[0], 0.5)
-    assert thresholded.shape == layer.data.shape
-    # etc.
+    widget = receiver_widget(viewer)
+    assert isinstance(widget, ReceiverWidget)
+    assert widget.viewer is viewer
 
 
-def test_image_threshold_widget(make_napari_viewer):
+def test_autocontrast_updates_layer_limits(make_napari_viewer):
     viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-    my_widget = ImageThreshold(viewer)
+    widget = receiver_widget(viewer)
 
-    # because we saved our widgets as attributes of the container
-    # we can set their values without having to "interact" with the viewer
-    my_widget._image_layer_combo.value = layer
-    my_widget._threshold_slider.value = 0.5
+    # simulate a received grayscale image without contrast_limits
+    arr = np.random.rand(32, 32).astype(np.float32)
+    meta = {"name": "foo", "rgb": False}
+    widget._on_received(arr, meta)
 
-    # this allows us to run our functions directly and ensure
-    # correct results
-    my_widget._threshold_im()
-    assert len(viewer.layers) == 2
-
-
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
-    viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
-
-    # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
-
-    # call our widget method
-    my_widget._on_click()
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
+    layer = viewer.layers["foo"]
+    assert layer.contrast_limits is not None
